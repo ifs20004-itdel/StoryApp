5ac565811a7b038aa759c75df603a346c03aaa9c
Samuel I. H. Sibuea <ifs20004@students.del.ac.id>
2023-05-02 10:06:24 +0700

    second progress

--- app/src/main/java/com/example/storyapp/MainActivity.kt
+++ app/src/main/java/com/example/storyapp/MainActivity.kt
@@ -1,11 +1,80 @@
 package com.example.storyapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.*
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
+import com.example.storyapp.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setupView()
+
+        val registerLabel  = binding?.registerLabel
+        var registerText= "Register"
+        val deviceLocal = Locale.getDefault().country
+        if(deviceLocal.equals("US")){
+            registerText = "Register"
+        }else if(deviceLocal.equals("ID")){
+            registerText = "Daftar"
+        }
+        registerLabel?.makeLinks(
+            Pair(registerText, View.OnClickListener {
+                Toast.makeText(applicationContext, "Register clicked", Toast.LENGTH_SHORT).show()
+            })
+        )
     }
+
+    private fun setupView(){
+        @Suppress("DEPRECATION")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>) {
+        val spannableString = SpannableString(this.text)
+        var startIndexOfLink = -1
+        for (link in links) {
+            val clickableSpan = object : ClickableSpan() {
+                override fun updateDrawState(textPaint: TextPaint) {
+                    textPaint.color = textPaint.linkColor
+                    textPaint.isUnderlineText = true
+                }
+                override fun onClick(view: View) {
+                    Selection.setSelection((view as TextView).text as Spannable, 0)
+                    view.invalidate()
+                    link.second.onClick(view)
+                }
+            }
+            startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
+            spannableString.setSpan(
+                clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+        this.movementMethod =
+            LinkMovementMethod.getInstance()
+        this.setText(spannableString, TextView.BufferType.SPANNABLE)
+    }
+
 }
